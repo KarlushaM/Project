a0511ce9fae13e3f42f16a9c3ba5f4a32a901b33
KarlushaM <dzevachka@mail.ru>
2025-08-27 21:09:16 +0300

    add Teacher

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Subjects
+from .models import Klass
+from .models import Teacher
+
+admin.site.register(Subjects)
+admin.site.register(Klass)
+admin.site.register(Teacher)
+
 
 # Register your models here.
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Subjects(models.Model):
+	name = models.CharField("Учебный предмет", max_length = 50)
+	
+	def __str__(self):
+		return f"{self.name}"
+	class Meta:
+		verbose_name_plural = "Учебные предметы"
+		verbose_name = "Учебный предмет"
+		
+class Klass(models.Model):
+	name = models.CharField("Класс", max_length = 2)
+	
+	def __str__(self):
+		return f"{self.name}"
+	class Meta:
+		verbose_name_plural = "Классы"
+		verbose_name = "Класс"
+		
+
+class Teacher(models.Model):
+
+	fio = models.CharField("ФИО", max_length=20)
+	description = models.TextField("Onиcaниe", null=True, blank=True)
+	price = models.TextField("Цена", null=True, blank=True)
+	subjects = models.ManyToManyField(Subjects, verbose_name="Учебный предмет")
+	author = models.ManyToManyField(Klass, verbose_name= "Класс")
+
+	def __str__(self):
+		return f"{self.fio}, {self.subjects}"
+	
+	class Meta:
+		verbose_name_plural = "Учителя"
+		verbose_name = "Учитель"
