bb373b92aace6360e0e08b859a977fd77420d72b
KarlushaM <dzevachka@mail.ru>
2025-09-01 21:25:41 +0300

    переход на учителку

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Teacher
 from django.views.generic import DetailView, ListView, View, CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 class TeachersListViews(ListView):
 	model = Teacher
@@ -18,6 +18,8 @@ class TeacherFormView(CreateView):
 	model = Teacher
 	fields = "__all__"
 	template_name ='teachers/teacher_form.html'
-	success_url = reverse_lazy('teachers_list')
+	def get_success_url(self):
+		return reverse("teacher_deatails", kwargs={"pk":self.object.pk})
+	
 
 
